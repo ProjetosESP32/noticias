b63e7836720733839cfbcec2057d80ec45478a93
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2023-05-09 16:55:29 -0400

    slideX keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./resources/**/*.{edge,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideX: {
+          '0%': {
+            transform: 'translateX(100%)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
